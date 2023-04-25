5c42856a053c5c854de385e668d1a74f8da4937d
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-04-25 13:31:46 -0500

    Updated installer file

--- install.tcl
+++ install.tcl
@@ -1,5 +1,5 @@
-package require tin 0.3.2
-set dir [tin mkdir -force wob-0.1]
+package require tin 0.4
+set dir [tin mkdir -force wob 0.1.3]
 file copy README.md $dir 
 file copy LICENSE $dir 
 file copy wob.tcl $dir 
