44b821e4fcf0303905ad30d5128affca76643d08
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-04-25 13:32:08 -0500

    Updated version

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {![package vsatisfies [package provide Tcl] 8.6]} {return}
-package ifneeded wob 0.1.2 [list source [file join $dir wob.tcl]]
+package ifneeded wob 0.1.3 [list source [file join $dir wob.tcl]]
