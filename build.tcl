1e87f4583c1268ce4c46e75e02d23aa8699a7b51
Alex Baker <ambaker1@mtu.edu>
2023-05-30 14:55:14 -0500

    Update build.tcl
    finished tests

--- build.tcl
+++ build.tcl
@@ -10,7 +10,16 @@ source build/pkgIndex.tcl
 tin import wob -exact $wob_version
 tin import tcltest
 
-# Close widgets
+# Ensure that widgets are initialized properly
+test widget {
+    # Ensures that the "title" argument works
+} -body {
+    set widget [widget new foo]
+    $widget eval {wm title .}
+} -result {foo}
+$widget destroy
+
+# create and close widgets
 test createWidgets {
     # Create widgets
 } -body {
@@ -27,16 +36,21 @@ test closeAllWidgets {
     llength [info class instances ::wob::widget]
 } -result {0}
 
-# Test out exitMainLoop
-
-test widgetTitle {
-    # Ensures that the "title" argument works
+test mainLoop_scope {
+    # Ensure that the mainLoop userInput is being executed in the proper scope
 } -body {
-    set widget [widget new foo]
-    $widget eval {wm title .}
-} -result {foo}
-$widget destroy
+    set x 0
+    after idle {
+        puts ""
+        set ::wob::userInput "set x 1"
+        set ::wob::userInputComplete 1
+        after idle exitMainLoop
+    }
+    mainLoop
+    set x
+} -result {1}
 
+# Test out exitMainLoop
 test exitMainLoop_1 {
     # Try out mainLoop and exitMainLoop
 } -body {
@@ -166,29 +180,7 @@ test widget_vlink_unset_aa2 {
     info exists x
 } -result {0}
 
-# test widget_vlink_write_array {
-    # # Test variable links
-# } -body {
-    # set widget [widget new]
-    # $widget vlink x x
-    # set x 10
-    # $widget get x
-# } -result {10}
-
-# test widget_vlink_read {
-    # # Test variable links
-# } -body {
-    # $widget set x 5
-    # set x
-# } -result {5}
-
-# test widget_vlink_unset {
-    # # Test variable links
-# } -body {
-    # unset x
-    # $widget eval info exists x
-# } -result {0}
-
+$widget destroy; # Clean up
 
 # Check number of failed tests
 set nFailed $::tcltest::numTests(Failed)
@@ -201,109 +193,11 @@ if {$nFailed > 0} {
     error "$nFailed tests failed"
 }
 
-exit
+# Final interactive mainLoop test
+puts "press enter to update and install files"
+mainLoop break
 
-# Test 2 (basic prompt, menu selection
-set widget [widget new]
-$widget eval {
-wm minsize . 250 50
-label .label -text "Choose analysis type:"
-tk_optionMenu .options AnalysisType "" Pushover Dynamic
-pack .label -side top -fill x
-pack .options -side bottom -fill x
-vwait AnalysisType
-}
-puts [$widget get AnalysisType]
-$widget destroy
-
-# Test 3 (Manipulate clipboard (can paste hello world into mainLoop))
-set widget [widget new]
-$widget set text "hello world"
-$widget eval {
-	clipboard clear
-	clipboard append $text
-}
-mainLoop; # Use "return" to continue
-catch {$widget destroy}
-
-# Test 3: Basic stopwatch
-# Code borrowed from https://wiki.tcl-lang.org/page/A+little+stopwatch
-set stopwatch [widget new]
-$stopwatch eval {
-    #! /usr/bin/tclsh
-    package require Tk
-    option add *Button.padY 0        ;# to make it look better on Windows
-    option add *Button.borderWidth 1
-    #---------------------------------------------------- testing i18n
-    package require msgcat
-    namespace import msgcat::mc msgcat::mcset
-    mcset de Start Los
-    mcset de Stop  Halt
-    mcset de Zero  Null
-    mcset fr Start Allez
-    mcset fr Stop  Arrêtez
-    mcset fr Zero  ???
-    mcset zh Start \u8DD1
-    mcset zh Stop  \u505C
-    mcset zh Zero  ???
-    msgcat::mclocale en ;# edit this line for display language
-    #--------------------------------------------------------------- UI
-    button .start -text [mc Start] -command Start
-    label  .time -textvar time -width 9 -bg black -fg yellow -font "Sans 20"
-    set time 00:00.00
-    button .stop -text [mc Stop] -command Stop
-    button .zero -text [mc Zero] -command Zero
-    set state 0
-    bind . <Key-space> {
-        if {$state} {.stop invoke
-        } else {
-            .start invoke
-        }
-    }
-    bind . <Key-0> {
-        .zero invoke
-    }
-    eval pack [winfo children .] -side left -fill y
-    #------------------------------------------------------- procedures
-    proc every {ms body} {eval $body; after $ms [namespace code [info level 0]]}
-
-    proc Start {} {
-        if {$::time eq {00:00.00}} {
-            set ::time0 [clock clicks -milliseconds]
-        }
-        every 20 {
-            set m [expr {[clock clicks -milliseconds] - $::time0}]
-            set ::time [format %2.2d:%2.2d.%2.2d \
-                [expr {$m/60000}] [expr {($m/1000)%60}] [expr {$m%1000/10}]]
-            incr ::titleskip
-            if {$::titleskip >= 12} {
-                wm title . "Timer $::time"
-                set ::titleskip 0
-            }
-        }
-        .start config -state disabled
-        set ::state 1
-    }
-    proc Stop {} {
-        if {[llength [after info]]} {
-            after cancel [after info]
-        }
-        .start config -state normal
-        set ::state 0
-    }
-    proc Zero {} {
-        set ::time 00:00.00
-        set ::time0 [clock clicks -milliseconds]
-    }
-}
-mainLoop; # Use "return" to continue
-catch {$stopwatch destroy}
-
-# Everything ok?
-puts "Update main files and install? (Y/N)"
-set result [gets stdin]
-if {$result eq "Y"} {
-    file copy -force {*}[glob -directory build *] [pwd]
-    tin bake doc/template/version.tin doc/template/version.tex $config
-    source install.tcl
-}
+# Update and install
+file copy -force {*}[glob -directory build *] [pwd]
+tin bake doc/template/version.tin doc/template/version.tex $config
+source install.tcl
